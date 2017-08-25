598776451d5a4cfddedb3968939b21f7ea2c069e
juan <juan@iMac-de-juan.local>
2017-08-25 16:51:50 -0500

    main structure

---
+++ swift_project/cvapp/cvapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cvapp
+//
+//  Created by juan on 25/08/17.
+//  Copyright © 2017 juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
